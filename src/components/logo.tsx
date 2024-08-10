04a22abb8c2e0fc2685ea8f7841388cfc267f959
costelnr1 <140845570+costelnr1@users.noreply.github.com>
2024-08-10 18:19:01 +0300

    Add favicon

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,10 @@
-import { Flex, Title } from "@mantine/core";
-import { IconGlobeFilled } from "@tabler/icons-react";
+import { Flex, Title } from '@mantine/core';
+import { IconGlobe } from '@tabler/icons-react';
 
 export function Logo() {
   return (
-    <Flex align={"center"} gap={4} justify={"center"}>
-      <IconGlobeFilled />
+    <Flex align={'center'} gap={4} justify={'center'}>
+      <IconGlobe />
       <Title fw={700} order={2} c="var(--mantine-white)">
         WORLDLE
       </Title>
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { MantineProvider } from "@mantine/core";
-import { Notifications } from "@mantine/notifications";
-import "@mantine/core/styles.css";
-import "@mantine/notifications/styles.css";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import '@mantine/core/styles.css';
+import '@mantine/notifications/styles.css';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
       <Notifications />
-      <App />
+      <main>
+        <App />
+      </main>
     </MantineProvider>
   </React.StrictMode>
 );
